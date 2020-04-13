4b2153febe308200eda43a0c775b2be33abb9d98
Ashish <ashishdudhe64@gmail.com>
2020-04-13 15:31:57 +0530

    add new consept

--- opencv2.py
+++ opencv2.py
@@ -7,6 +7,14 @@ def canny(image):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
+def display_line(image,line):
+    line_im=np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2=line.reshape(4)
+            cv2.line(line_im,(x1,y1),(x2,y2),(255,0,0),10)
+    return line_im
+
 
 def region_of_in(image):
     height=image.shape[0]
@@ -23,5 +31,8 @@ image=cv2.imread("test_image.jpg")
 image_le= np.copy(image)
 canny=canny(image_le)
 cropped_im=region_of_in(canny)
-cv2.imshow("resum",cropped_im )
+lines=cv2.HoughLinesP(cropped_im,2,np.pi/180, 100,np.array([]),minLineLength=40,maxLineGap=5)
+line_im=display_line(image_le,lines)
+combo=cv2.addWeighted(image_le,0.8,line_im,1,1)
+cv2.imshow("resum",combo)
 cv2.waitKey(0)
